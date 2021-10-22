2c5631f354c6c965418b11f6bdf8e0b7970cc550
Artym Litvinenko <nixart986@gmail.com>
2021-10-22 13:38:49 +0300

    Add enumerate and insert

--- main.py
+++ main.py
@@ -5,26 +5,22 @@ def rev(string):
     for word in words:
         symbol = {}
         letters = []
-        for i in range(len(word)):
-            if not word[i].isalpha():
-                symbol[i] = word[i]
+        for i, char in enumerate(word):
+            if not char.isalpha():
+                symbol[i] = char
             else:
-                letters.append(word[i])
+                letters.append(char)
         reversed_word = letters[::-1]
         for key, value in symbol.items():
-            reversed_word = "".join(reversed_word)
-            reversed_word = reversed_word[:key] + value + reversed_word[key:]
-        reversed_words.append("".join(reversed_word))
+            reversed_word.insert(key, value)
     return " ".join(reversed_words)
 
 
 if __name__ == '__main__':
     cases = [
-
         ("abcd efgh", "dcba hgfe"),
         ("a1bcd efg!h", "d1cba hgf!e"),
         ("", ""),
-
     ]
     for text, reversed_text in cases:
         assert rev(text) == reversed_text
