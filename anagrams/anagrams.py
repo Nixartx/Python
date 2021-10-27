6bfb5838e4171a051a3bb252f8e7abd4483792d9
Artym Litvinenko <nixart986@gmail.com>
2021-10-27 13:36:34 +0300

    TimeComplexity fix for string_reverse

--- anagrams/anagrams.py
+++ anagrams/anagrams.py
@@ -3,15 +3,15 @@ def string_reverse(string):
     words = string.split()
     reversed_words = []
     for word in words:
-        symbol = {}
-        letters = []
-        for i, char in enumerate(word):
-            if not char.isalpha():
-                symbol[i] = char
-            else:
+        reversed_word=[]
+        letters=[]
+        for char in word:
+            if char.isalpha():
                 letters.append(char)
-        reversed_word = letters[::-1]
-        for key, value in symbol.items():
-            reversed_word.insert(key, value)
+        for char in word:
+            if char.isalpha():
+                reversed_word.append(letters.pop())
+            else:
+                reversed_word.append(char)
         reversed_words.append(''.join(reversed_word))
     return " ".join(reversed_words)
