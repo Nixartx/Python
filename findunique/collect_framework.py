7c4f15470d59316e6ef6a00354b2364497b05d94
Artym Litvinenko <nixart986@gmail.com>
2021-11-05 16:13:50 +0200

    Task 4 - CLI (Fix2)

--- findunique/collect_framework.py
+++ findunique/collect_framework.py
@@ -12,22 +12,20 @@ class CLCountUnique(CountUnique):
         self.__parser.add_argument("--file", type=str, help="Path to file")
 
     def readfile(self):
-        f = open(self.__args.file)
-        lines = f.readlines()
-        f.close()
-        return lines
+        try:
+            with open(self.__args.file) as f:
+                lines = f.readlines()
+            return lines
+        except IOError:
+            print("Error: File does not appear to exist.")
 
-    def run(self, args=0):
-        if args:
-            self.__args = self.__parser.parse_args(args)
-        else:
-            self.__args = self.__parser.parse_args()
-
+    def run(self, args=None):
+        self.__args = self.__parser.parse_args(args)
         if self.__args.file:
-            for key, line in enumerate(self.readfile()):
+            for key, line in enumerate(self.readfile(), 1):
                 print(
                     "Строка: {0}. Уникальных символов: {1}".format(
-                        key + 1, self.count(line)))
+                        key, self.count(line)))
         elif self.__args.string:
             print(
                 "Уникальных символов: {0}".format(
