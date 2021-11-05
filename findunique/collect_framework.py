8b6993e46bbf42a1fd9f05adc0cc199172f86ce7
Artym Litvinenko <nixart986@gmail.com>
2021-11-05 17:06:49 +0200

    Task 4 - CLI (Fix and add test_CLCountUniqueCase_file_exception)

--- findunique/collect_framework.py
+++ findunique/collect_framework.py
@@ -12,22 +12,27 @@ class CLCountUnique(CountUnique):
         self.__parser.add_argument("--file", type=str, help="Path to file")
 
     def readfile(self):
-        try:
+
             with open(self.__args.file) as f:
                 lines = f.readlines()
             return lines
-        except IOError:
-            print("Error: File does not appear to exist.")
+
 
     def run(self, args=None):
-        self.__args = self.__parser.parse_args(args)
-        if self.__args.file:
-            for key, line in enumerate(self.readfile(), 1):
+        try:
+            self.__args = self.__parser.parse_args(args)
+            if self.__args.file:
+                for key, line in enumerate(self.readfile(), 1):
+                    print(
+                        "Строка: {0}. Уникальных символов: {1}".format(
+                            key, self.count(line)))
+            elif self.__args.string:
                 print(
-                    "Строка: {0}. Уникальных символов: {1}".format(
-                        key, self.count(line)))
-        elif self.__args.string:
-            print(
-                "Уникальных символов: {0}".format(
-                    self.count(
-                        self.__args.string)))
+                    "Уникальных символов: {0}".format(
+                        self.count(
+                            self.__args.string)))
+        except FileNotFoundError:
+            print("Error: File does not appear to exist.")
+
+if __name__ == "__main__":
+    CLCountUnique().run(['--file', 'abc'])
--- tests/command_line_tests.py
+++ tests/command_line_tests.py
@@ -1,3 +1,4 @@
+import pytest
 from findunique import CLCountUnique
 from unittest.mock import patch, mock_open
 
@@ -23,3 +24,8 @@ def test_CLCountUniqueCase_String_and_File(capfd):
         CLCountUnique().run(['--file', 'path_to_file', '--string', 'abcd'])
         actual, err = capfd.readouterr()
         assert actual == expected
+
+def test_CLCountUniqueCase_file_exception():
+    expected = "Error: File does not appear to exist."
+    with pytest.raises(FileNotFoundError):
+        CLCountUnique().run(['--file', 'abc'])
