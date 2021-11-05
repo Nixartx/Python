2647a3cdee599ec73559f250632118e57d6ed528
Artym Litvinenko <nixart986@gmail.com>
2021-11-05 17:11:05 +0200

    Task 4 - CLI (Fix and add test_CLCountUniqueCase_file_exception)

--- findunique/collect_framework.py
+++ findunique/collect_framework.py
@@ -3,34 +3,34 @@ import argparse
 
 
 class CLCountUnique(CountUnique):
-    __parser = 0
-    __args = 0
+    parser = 0
+    _args = 0
 
     def __init__(self):
-        self.__parser = argparse.ArgumentParser()
-        self.__parser.add_argument("--string", type=str)
-        self.__parser.add_argument("--file", type=str, help="Path to file")
+        self.parser = argparse.ArgumentParser()
+        self.parser.add_argument("--string", type=str)
+        self.parser.add_argument("--file", type=str, help="Path to file")
 
     def readfile(self):
 
-            with open(self.__args.file) as f:
+            with open(self._args.file) as f:
                 lines = f.readlines()
             return lines
 
 
     def run(self, args=None):
         try:
-            self.__args = self.__parser.parse_args(args)
-            if self.__args.file:
+            self._args = self.parser.parse_args(args)
+            if self._args.file:
                 for key, line in enumerate(self.readfile(), 1):
                     print(
                         "Строка: {0}. Уникальных символов: {1}".format(
                             key, self.count(line)))
-            elif self.__args.string:
+            elif self._args.string:
                 print(
                     "Уникальных символов: {0}".format(
                         self.count(
-                            self.__args.string)))
+                            self._args.string)))
         except FileNotFoundError:
             print("Error: File does not appear to exist.")
 
--- tests/command_line_tests.py
+++ tests/command_line_tests.py
@@ -26,6 +26,6 @@ def test_CLCountUniqueCase_String_and_File(capfd):
         assert actual == expected
 
 def test_CLCountUniqueCase_file_exception():
-    expected = "Error: File does not appear to exist."
+    #expected = "Error: File does not appear to exist."
     with pytest.raises(FileNotFoundError):
         CLCountUnique().run(['--file', 'abc'])
