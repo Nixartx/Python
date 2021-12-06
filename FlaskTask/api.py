53accd83c896d0bf573ab8ccbb0c2b3e92e331d7
Artym Litvinenko <nixart986@gmail.com>
2021-12-06 17:18:59 +0200

    Task 8 Fixes

--- FlaskTask/api.py
+++ FlaskTask/api.py
@@ -5,11 +5,11 @@ from flask import Flask, request, make_response
 from flask_restful import Resource, Api
 from ReportOfMonaco import ReportMonaco
 
+
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     api = Api(app)
-
 
     app.config.from_mapping(
         SECRET_KEY=os.environ.get('SECRET_KEY', 'dev'),
@@ -31,14 +31,12 @@ def create_app(test_config=None):
 
     data = ReportMonaco().build_report(folder='static/reports')
 
-
     @api.representation('application/json')
     def output_json(data, code, headers=None):
         resp = make_response(json.dumps({'response': data}, default=str), code)
         resp.headers.extend(headers or {})
         return resp
 
-    api.representations['application/json'] = output_json
 
     @api.representation('application/xml')
     def output_xml(data, code, headers=None):
@@ -46,45 +44,24 @@ def create_app(test_config=None):
         resp.headers.extend(headers or {})
         return resp
 
-    api.representations['application/xml'] = output_xml
 
-
-    class List(Resource):
+    class Report(Resource):
         def get(self):
             driver_id = request.args.get('driver_id')
-
             order = request.args.get('order', 'asc')
             reverse = order.upper() == 'DESC'
-
-            if order and order.upper() == 'DESC':
-                order = True
-            else:
-                order = False
-
             if driver_id:
                 reports = data.generate_report(
                     driver_id=driver_id,
-                    reverse=order)
+                    reverse=reverse)
                 return reports
             reports = data.generate_report(reverse=reverse)
             return reports
 
-    class Report(Resource):
-        def get(self):
-            order = request.args.get('order', 'asc')
-            reverse = order.upper() == 'DESC'
-            format=request.args.get('format','json')
-
-            reports = data.generate_report(reverse=reverse)
-            if format=='json':
-                return reports, 200, {'Accept':'application/json', 'Content-Type': 'application/json'}
-            else:
-                return reports, 200, {'Accept':'application/xml', 'Content-Type': 'application/xml'}
-
     api.add_resource(Report, '/api/report/')
-    api.add_resource(List, '/api/report/drivers/')
 
     return app
+
 
 if __name__ == '__main__':
     app = create_app()
--- FlaskTask/test_api.py
+++ FlaskTask/test_api.py
@@ -2,46 +2,39 @@ import pytest
 from FlaskTask.api import create_app
 from .test_vars import *
 
+
 @pytest.fixture
 def client():
     app = create_app({'TESTING': True})
     client = app.test_client()
     return client
 
+
 cases = [
     ('/api/report/', 200, out1),
-    ('/api/report/drivers/', 200, out1),
-    ('/api/report/drivers/?driver_id=SVF', 200, out2),
-    ('/api/report/drivers/?order=desc', 200, out3)
+    ('/api/report/?driver_id=SVF', 200, out2),
+    ('/api/report/?order=desc', 200, out3)
 ]
 
 cases_xml = [
     ('/api/report/', 200, xml_out1),
-    ('/api/report/drivers/', 200, xml_out1),
-    ('/api/report/drivers/?driver_id=SVF', 200, xml_out2),
-    ('/api/report/drivers/?order=desc', 200, xml_out3),
+    ('/api/report/', 200, xml_out1),
+    ('/api/report/?driver_id=SVF', 200, xml_out2),
+    ('/api/report/?order=desc', 200, xml_out3),
 ]
 
-cases_format=[
-    ('/api/report/drivers/?format=json','application/json'),
-    ('/api/report/drivers/?format=xml','application/xml'),
-]
 
 @pytest.mark.parametrize('param,expected_code,expected_json', cases)
-def test_post_json(client,param,expected_code,expected_json):
-    result=client.get(param, headers={"Accept":"application/json"})
-    assert result.is_json==True
-    assert result.get_json()['response']==expected_json
-    assert result.status_code==expected_code
+def test_post_json(client, param, expected_code, expected_json):
+    result = client.get(param, headers={"Accept": "application/json"})
+    assert result.is_json
+    assert result.get_json()['response'] == expected_json
+    assert result.status_code == expected_code
+
 
 @pytest.mark.parametrize('param,expected_code,expected_xml', cases_xml)
-def test_post_xml(client,param,expected_code,expected_xml):
-    result=client.get(param, headers={"Accept":"application/xml"})
-    assert result.is_json==False
-    assert result.get_data()==expected_xml
-    assert result.status_code==expected_code
-
-@pytest.mark.parametrize('param,expected_format', cases_format)
-def test_format(client,param,expected_format):
-    result=client.get(param)
-    assert result.content_type==expected_format
+def test_post_xml(client, param, expected_code, expected_xml):
+    result = client.get(param, headers={"Accept": "application/xml"})
+    assert result.is_json == False
+    assert result.get_data() == expected_xml
+    assert result.status_code == expected_code
