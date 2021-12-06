697a55a96e19d5b3c127d3afed4bac56475e2ca2
Artym Litvinenko <nixart986@gmail.com>
2021-12-06 16:25:16 +0200

    Task 8

--- FlaskTask/test_api.py
+++ FlaskTask/test_api.py
@@ -15,16 +15,23 @@ cases = [
     ('/api/report/drivers/?order=desc', 200, out3)
 ]
 
+cases_xml = [
+    ('/api/report/', 200, xml_out1),
+    ('/api/report/drivers/', 200, xml_out1),
+    ('/api/report/drivers/?driver_id=SVF', 200, xml_out2),
+    ('/api/report/drivers/?order=desc', 200, xml_out3),
+]
+
 @pytest.mark.parametrize('param,expected_code,expected_json', cases)
 def test_post_json(client,param,expected_code,expected_json):
-    result=client.get(param)
+    result=client.get(param, headers={"Accept":"application/json"})
     assert result.is_json==True
     assert result.get_json()['response']==expected_json
     assert result.status_code==expected_code
 
-# @pytest.mark.parametrize('param,expected_code,expected_xml', cases)
-# def test_post_xml(client,param,expected_code,expected_xml):
-#     result=client.get(param)
-#     assert result.is_json==False
-#     assert result.get_data()['response']==expected_xml
-#     assert result.status_code==expected_code
+@pytest.mark.parametrize('param,expected_code,expected_xml', cases_xml)
+def test_post_xml(client,param,expected_code,expected_xml):
+    result=client.get(param, headers={"Accept":"application/xml"})
+    assert result.is_json==False
+    assert result.get_data()==expected_xml
+    assert result.status_code==expected_code
